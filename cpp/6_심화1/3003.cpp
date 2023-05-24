4aa1caa471c6d876ff3ca3da57fcecbd30ed2bdc
kwongholee <ghlee9883@gmail.com>
2023-05-24 17:59:13 +0900

    5.24

--- cpp/6_심화1/3003.cpp
+++ cpp/6_심화1/3003.cpp
@@ -9,5 +9,15 @@ int main() {
 	cin.tie(NULL);
 	ios_base::sync_with_stdio(false);
 
-	
+	int a, b, c, d, e, f;
+	cin >> a >> b >> c >> d >> e >> f;
+	vector<int> v1 = { 1,1,2,2,2,8 };
+	vector<int> v2;
+	v2.push_back(a);
+	v2.push_back(b);
+	v2.push_back(c);
+	v2.push_back(d);
+	v2.push_back(e);
+	v2.push_back(f);
+	for (int i = 0; i < 6; i++) cout << v1[i] - v2[i] << " ";
 }
