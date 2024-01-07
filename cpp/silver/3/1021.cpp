853b7f2027f03e3219f4b5f8653eba1b912a5c19
kwongholee <ghlee9883@gmail.com>
2024-01-07 14:39:09 +0900

    [add] 01.07

--- cpp/silver/3/1021.cpp
+++ cpp/silver/3/1021.cpp
@@ -11,7 +11,7 @@
 
 using namespace std;
 
-int N, K, input[50], two_cnt = 0, three_cnt = 0;
+int N, K, ans = 0, two_cnt = 0, three_cnt = 0;
 deque<int> dq;
 
 int main() {
@@ -21,24 +21,38 @@ int main() {
 
 	cin >> N >> K;
 	for (int i = 1; i <= N; i++) dq.push_back(i);
-	for (int i = 0; i < K; i++) cin >> input[i];
-	for (int i = 0; i < K; i++) {
-		int j = input[i] - 1 - i - two_cnt + three_cnt;
-		if (dq.size() / 2 > j) {
-			for (int k = 0; k < j; k++) {
+	while (K--) {
+		int num;
+		cin >> num;
+		for (int i = 0; i < dq.size(); i++) {
+			if (dq[i] == num) {
+				two_cnt = i;
+				three_cnt = dq.size() - i;
+			}
+		}
+		if (two_cnt <= three_cnt) {
+			while (true) {
+				if (dq.front() == num) {
+					dq.pop_front();
+					break;
+				}
 				dq.push_back(dq.front());
 				dq.pop_front();
-				two_cnt++;
+				ans++;
 			}
 		}
 		else {
-			for (int k = 0; k < dq.size()-j; k++) {
+			ans++;
+			while (true) {
+				if (dq.back() == num) {
+					dq.pop_back();
+					break;
+				}
 				dq.push_front(dq.back());
 				dq.pop_back();
-				three_cnt++;
+				ans++;
 			}
 		}
-		dq.pop_front();
 	}
-	cout << two_cnt + three_cnt;
+	cout << ans;
 }
